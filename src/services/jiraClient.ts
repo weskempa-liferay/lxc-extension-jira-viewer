d549b9d77028325f33e6e1109f68e07f22690fa4
Keven Leone <keven.leone@liferay.com>
2023-03-12 14:21:14 -0300

    Add Jira Client

---
+++ src/services/jiraClient.ts
@@ -0,0 +1,21 @@
+import { Version2Client, Version3Client, Config } from 'jira.js';
+
+const apiToken = process.env.JIRA_API_TOKEN;
+const apiVersion = process.env.JIRA_CLIENT_VERSION;
+const host = process.env.JIRA_HOST;
+const password = process.env.JIRA_API_PASSWORD;
+const username = process.env.JIRA_API_USERNAME;
+
+const VersionClient = apiVersion === 'v3' ? Version3Client : Version2Client;
+
+const jiraClient = new VersionClient({
+  host,
+  authentication: {
+    basic: { username, password, apiToken },
+  },
+  newErrorHandling: true,
+} as Config);
+
+// Exporting as V3, to avoid typing issue
+
+export default jiraClient as Version3Client;
